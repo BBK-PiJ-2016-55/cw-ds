9c2e0e10f5b92f1e7bf37c0ee0601037a63ba238
Sarah <svince04@mail.bbk.ac.uk>
2016-12-30 13:36:02 +0000

    first commit

---
+++ src/ArrayList.java
@@ -0,0 +1,35 @@
+public class ArrayList implements List {
+	
+	private int size = 0;
+	private static int START_SIZE = 20; 
+	private Object[] array = new Object[START_SIZE];
+
+	public boolean isEmpty() {
+		return (size == 0);
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public ReturnObject get(int index) {
+		ReturnObject firstObject = null;
+		return firstObject;
+	}
+
+	public ReturnObject remove(int index) {
+		ReturnObject firstObject = null;
+		return firstObject;
+	}
+
+	public ReturnObject add(int index, Object item) {
+		ReturnObject firstObject = null;
+		return firstObject;
+	}
+
+	public ReturnObject add(Object item) {
+		ReturnObject firstObject = null;
+		return firstObject;
+	}
+
+} 
