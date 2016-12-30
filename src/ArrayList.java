e9d6fef4693ebc2f2c4089e33fd456fbe4ff63e7
Sarah <svince04@mail.bbk.ac.uk>
2016-12-30 14:04:26 +0000

    get() added

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -3,6 +3,7 @@ public class ArrayList implements List {
 	private int size = 0;
 	private static int START_SIZE = 20; 
 	private Object[] array = new Object[START_SIZE];
+	private ReturnObject retObject;
 
 	public boolean isEmpty() {
 		return (size == 0);
@@ -13,8 +14,14 @@ public class ArrayList implements List {
 	}
 
 	public ReturnObject get(int index) {
-		ReturnObject firstObject = null;
-		return firstObject;
+		if (isEmpty()) {
+			retObject = new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		} else if (index < 0 || index > size) {
+			retObject = new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else {
+			retObject = new ReturnObjectImpl(array[index]);
+		}	
+	return retObject;
 	}
 
 	public ReturnObject remove(int index) {
