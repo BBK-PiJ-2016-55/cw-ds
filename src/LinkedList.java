0ad7015afe4abe648414c2ed3d2a2e4b4c05810e
Sarah <svince04@mail.bbk.ac.uk>
2016-12-31 18:25:27 +0000

    Added javadoc and tidied up comments

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -8,25 +8,34 @@ public class LinkedList implements List {
 	private Node firstNode = null;
 	private ReturnObject retObject;
 
+	/**
+	 * Returns true if the list is empty, false otherwise. 
+	 * 
+	 * @return true if the list is empty, false otherwise. 
+	 */
 	public boolean isEmpty() {
 		return (size == 0);
 	}
 
+	/**
+	 * Returns the number of items currently in the list.
+	 * 
+	 * @return the number of items currently in the list
+	 */
 	public int size() {
 		return size;
 	}
 
-	public ReturnObject errorCheck(int index) {
-		if (isEmpty()){
-			retObject = new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
-		} else if (index < 0 || index >= size) {
-			retObject = new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
-		} else {
-			retObject = new ReturnObjectImpl(ErrorMessage.NO_ERROR);
-		}
-		return retObject;
-	}
-
+	/**
+	 * Returns the element at the given position. 
+	 * 
+	 * If the index is negative or greater or equal than the size of
+	 * the list, then an appropriate error must be returned.
+	 * 
+	 * @param index the position in the list of the item to be retrieved
+	 * @return the element or an appropriate error message, 
+	 *         encapsulated in a ReturnObject
+	 */
 	public ReturnObject get(int index){
 		// Check for errors
 		retObject = errorCheck(index);
@@ -42,82 +51,142 @@ public class LinkedList implements List {
 		return retObject;
 	}
 
+	/**
+	 * Returns the elements at the given position and removes it
+	 * from the list. The indeces of elements after the removed
+	 * element must be updated accordignly.
+	 * 
+	 * If the index is negative or greater or equal than the size of
+	 * the list, then an appropriate error must be returned.
+	 * 
+	 * @param index the position in the list of the item to be retrieved
+	 * @return the element or an appropriate error message, 
+	 *         encapsulated in a ReturnObject
+	 */
 	public ReturnObject remove(int index){
-		retObject = get(index);
+		// Check for errors
+		retObject = errorCheck(index);
 		if (!retObject.hasError()) {
-			Node currentNode = firstNode;
-			// go through nodes until we reach the one before the one to be removed
-			// set that nextNode to point to the one *after* node for deletion
-			// decrease size
+			// If index is first item in list, redefine firstNode
 			if (index == 0) {
 				firstNode = firstNode.getNextNode();
-				size--;
 			} else {
+				Node currentNode = firstNode;
+				// Go through list to find item preceding index to be removed
 				for (int i = 0; i < (index - 1); i++) {
 					currentNode = currentNode.getNextNode();
 					}
+				// If index is last in list, set pointer to null
 				if (currentNode.getNextNode().getNextNode() == null){
 						currentNode.setNextNode(null);
+				// Otherwise, set pointer to item after deleted item
 				} else {
 						currentNode.setNextNode(currentNode.getNextNode().getNextNode());
 				}
-				size--;
-			}	
+			}
+		size--;
 		}
 		return retObject;
 	}
 
+	/**
+	 * Adds an element to the list, inserting it at the given
+	 * position. The indeces of elements at and after that position
+	 * must be updated accordignly.
+	 * 
+	 * If the index is negative or greater or equal than the size of
+	 * the list, then an appropriate error must be returned.
+	 * 
+	 * If a null object is provided to insert in the list, the
+	 * request must be ignored and an appropriate error must be
+	 * returned.
+	 * 
+	 * @param index the position at which the item should be inserted in
+	 *              the list
+	 * @param item the value to insert into the list
+	 * @return an ReturnObject, empty if the operation is successful
+	 *         or containing an appropriate error message otherwise
+	 */
 	public ReturnObject add(int index, Object item) {
+		// Check for errors
+		retObject = errorCheck(index);
 		if (item == null) {
 			retObject = new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
-		} else if (index < 0 || index >= size) {
-			retObject = new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+			return retObject;
+		} else if (retObject.hasError()) {
+			return retObject;
 		} else {
-			// create new node with item passed in params
 			Node newNode = new Node(item);
 			retObject = new ReturnObjectImpl(item);
-			// adding to start of list
+			// If index is first item in list, redefine firstNode
 			if (index == 0) {
 				newNode.setNextNode(firstNode);
 				firstNode = newNode;
-			// adding anywhere else in list (except at end - out of bounds)
 			} else {
 				Node currentNode = firstNode;
-				// iterate through list to find + stop at item *before* one given in index
+				// Go through list to find item preceding index addition point
 				for (int i = 0; i < (index - 1); i++) {
 						currentNode = currentNode.getNextNode();
 					}
-				// set nextNode of newNode to point to that node's current nextNode value
-				newNode.setNextNode(currentNode.getNextNode());
-				// set NextNode of currentNode to point to newNode  
+				newNode.setNextNode(currentNode.getNextNode()); 
 				currentNode.setNextNode(newNode);
 			}
 		size++;
-		}	
 		return retObject;
+		}	
 	}
 
+	/**
+	 * Adds an element at the end of the list.
+	 * 
+	 * If a null object is provided to insert in the list, the
+	 * request must be ignored and an appropriate error must be
+	 * returned.
+	 * 
+	 * @param item the value to insert into the list
+	 * @return an ReturnObject, empty if the operation is successful
+	 *         or containing an appropriate error message otherwise
+	 */
 	public ReturnObject add(Object item) {
 		if (item == null) {
 			retObject = new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
 		} else {
 		Node newNode = new Node(item);
 		retObject = new ReturnObjectImpl(item);
-			// checks if list is empty and sets firstnode to new if so
+			// If list is empty, set firstNode
 			if (isEmpty()){
 				firstNode = newNode;
-				size++;
 			} else {
 				Node currentNode = firstNode;
-				// go to last node in list
+				// Find last node in list
 				while (currentNode.getNextNode() != null) {
 					currentNode = currentNode.getNextNode();
 				}
-				// set current last node to point to new node
 				currentNode.setNextNode(newNode);
-				size++;
 			}
+		size++;
 		}
 		return retObject;		
+	}
+
+	/**
+	 * Checks for errors related to indeces.
+	 * 
+	 * If an invalid index is entered, the appropriate error
+	 * message is returned.
+	 *
+	 * @param index the position in the index being validated
+	 * @return a ReturnObject, containing an appropriate error/
+	 *         no error message.
+	 */
+		public ReturnObject errorCheck(int index) {
+		if (isEmpty()){
+			retObject = new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		} else if (index < 0 || index >= size) {
+			retObject = new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else {
+			retObject = new ReturnObjectImpl(ErrorMessage.NO_ERROR);
+		}
+		return retObject;
 	}
 }
