cd21d92c696b7f4dfccc161f3f610c37530dcd31
Sarah <svince04@mail.bbk.ac.uk>
2016-12-31 12:04:20 +0000

    first commit

---
+++ src/LinkedList.java
@@ -0,0 +1,36 @@
+/**
+ * An implementation of {@see List} using an Object array.
+ * 
+ * @author svince04
+ */
+public class LinkedList implements List {
+
+	public boolean isEmpty() {
+		return false;
+	}
+
+	public int size() {
+		return 1;
+	}
+
+	public ReturnObject get(int index){
+		ReturnObject retObject = null;
+		return retObject;
+
+	}
+
+	public ReturnObject remove(int index){
+		ReturnObject retObject = null;
+		return retObject;
+	}
+
+	public ReturnObject add(int index, Object item) {
+		ReturnObject retObject = null;
+		return retObject;	
+	}
+
+	public ReturnObject add(Object item) {
+		ReturnObject retObject = null;
+		return retObject;			
+	}
+}
