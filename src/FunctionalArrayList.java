f739778cb9a8250a7b65db72191177ec4912750a
Sarah <svince04@mail.bbk.ac.uk>
2017-01-02 13:02:37 +0000

    added rest() and tidied comments

--- src/FunctionalArrayList.java
+++ src/FunctionalArrayList.java
@@ -43,9 +43,11 @@ public class FunctionalArrayList extends ArrayList implements FunctionalList {
      */
     public FunctionalList rest(){
     	FunctionalList funList = new FunctionalArrayList();
+        // Check for empty/head-only list
     	if (super.isEmpty() || (super.size() == 1)) {
     		return funList;
     	} else {
+            // Assign each element to new list, starting after head
     		for (int i = 1; i < (super.size()); i++) {
     			funList.add(super.get(i).getReturnValue()); 
     		}
--- src/FunctionalLinkedList.java
+++ src/FunctionalLinkedList.java
@@ -20,9 +20,25 @@ public class FunctionalLinkedList extends LinkedList implements FunctionalList {
 
 	}
 
+    /**
+     * Returns a list with the elements in this list except the
+     * head. The elements must be in the same order. The original list
+     * must not change or be affected by changes in the new list. 
+     * 
+     * If the list is empty, another empty list is returned. 
+     */
 	public FunctionalList rest() {
 		FunctionalList funList = new FunctionalLinkedList();
-		return funList;
+		// Check for empty/head-only list
+		if (super.isEmpty() || (super.size() == 1)) {
+    		return funList;
+    	} else {
+    		// Assign each element to new list, starting after head
+    		for (int i = 1; i < (super.size()); i++) {
+    			funList.add(super.get(i).getReturnValue()); 
+    		}
+    		return funList;
+    	}
 	}
 
 }
