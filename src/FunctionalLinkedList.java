42e8813e88015ac062aef9be716fe702d23c8cda
Sarah <svince04@mail.bbk.ac.uk>
2017-01-02 12:49:29 +0000

    added get() - needs testing

--- src/FunctionalLinkedList.java
+++ src/FunctionalLinkedList.java
@@ -4,7 +4,18 @@ public class FunctionalLinkedList extends LinkedList implements FunctionalList {
 		super();
 	}
 
+	/**
+     * Returns the element at the beginning of the list. 
+     * 
+     * If the list is empty, an appropriate error is returned. 
+     *
+     * @return a copy of the element at the beginning of the list or 
+     *         an error if the list is empty.
+     */
 	public ReturnObject head() {
+		// test to check if retObject needs to be assigned back - shouldn't be
+		// so 'retObject =' could be removed...
+		retObject = super.get(0);
 		return retObject;
 
 	}
