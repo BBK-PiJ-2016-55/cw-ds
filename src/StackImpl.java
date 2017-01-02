87af0082a0cf8f0a2d32802a5f1c73f4eaef3d0f
Sarah <svince04@mail.bbk.ac.uk>
2017-01-02 19:25:54 +0000

    added javadoc

--- src/StackImpl.java
+++ src/StackImpl.java
@@ -1,13 +1,30 @@
+/**
+ * A stack accepting {@see List} objects.
+ * Extends {@see AbstractStack}
+ * Uses index 0 as top of stack.
+ *
+ * @author svince04
+ */
 public class StackImpl extends AbstractStack {
 
 	public StackImpl(List list) {
 	  super(list);
 	}
 
+	/**
+	 * Returns true if the list is empty, false otherwise. 
+	 * 
+	 * @return true if the list is empty, false otherwise. 
+	 */
 	public boolean isEmpty() {
 		return (internalList.isEmpty());
 	}
 
+	/**
+	 * Returns the number of items currently in the list.
+	 * 
+	 * @return the number of items currently in the list
+	 */
 	public int size() {
 		return internalList.size();
 	}
@@ -35,7 +52,7 @@ public class StackImpl extends AbstractStack {
 
 	/**
 	 * Returns the element at the top of the stack. The element is
-	 * removed frmo the stack.
+	 * removed from the stack.
 	 * 
 	 * @return If stack is not empty, the item on the top is returned. If the
 	 *         stack is empty, an appropriate error.
