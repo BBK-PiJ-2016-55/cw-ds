240d4bd95e072c42f58e0af4e47fa912c3bf8a39
Sarah <svince04@mail.bbk.ac.uk>
2017-01-02 17:10:58 +0000

    Added List methods - needs testing

--- src/StackImpl.java
+++ src/StackImpl.java
@@ -5,26 +5,44 @@ public class StackImpl extends AbstractStack {
 	}
 
 	public boolean isEmpty() {
-		boolean boo = false;
+		boolean boo = (internalList.isEmpty());
 		return boo;
 	}
 
 	public int size() {
-		int size = 0;
-		return size;
+		return internalList.size();
 	}
 
+	/**
+	 * Adds an element at the top of the stack. 
+	 * 
+	 * @param item the new item to be added
+	 */
 	public void push(Object item) {
-
+		internalList.add(0, item);
 	}
 	
+	/**
+	 * Returns the element at the top of the stack. The stack is
+	 * left unchanged.
+	 * 
+	 * @return If stack is not empty, the item on the top is returned. If the
+	 *         stack is empty, an appropriate error.
+	 */
 	public ReturnObject top() {
-		ReturnObject retObject = null;
+		ReturnObject retObject = internalList.get(0);
 		return retObject;
 	}
 
+	/**
+	 * Returns the element at the top of the stack. The element is
+	 * removed frmo the stack.
+	 * 
+	 * @return If stack is not empty, the item on the top is returned. If the
+	 *         stack is empty, an appropriate error.
+	 */
 	public ReturnObject pop() {
-		ReturnObject retObject = null;
+		ReturnObject retObject = internalList.remove(0);
 		return retObject;
 	}
 
