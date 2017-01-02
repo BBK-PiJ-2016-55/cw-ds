4f2fad95657e095295e5ee04bdca990e5f565e0c
Sarah <svince04@mail.bbk.ac.uk>
2017-01-02 16:34:36 +0000

    first commit

---
+++ src/StackImpl.java
@@ -0,0 +1,31 @@
+public class StackImpl extends AbstractStack {
+
+	public StackImpl(List list) {
+	  super(list);
+	}
+
+	public boolean isEmpty() {
+		boolean boo = false;
+		return boo;
+	}
+
+	public int size() {
+		int size = 0;
+		return size;
+	}
+
+	public void push(Object item) {
+
+	}
+	
+	public ReturnObject top() {
+		ReturnObject retObject = null;
+		return retObject;
+	}
+
+	public ReturnObject pop() {
+		ReturnObject retObject = null;
+		return retObject;
+	}
+
+}
