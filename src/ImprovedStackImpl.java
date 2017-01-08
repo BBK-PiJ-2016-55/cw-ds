bc513aaef06b6932c199bca62ccecc7747869f8f
Sarah <svince04@mail.bbk.ac.uk>
2017-01-08 16:42:28 +0000

    removed superfluous javadoc

--- src/ImprovedStackImpl.java
+++ src/ImprovedStackImpl.java
@@ -1,22 +1,11 @@
 /**
- * An implementation of a stack with additional methods. 
+ * An implementation of {@see ImprovedStack}.
  * 
- * Classes implementing this interface must use a {@see List} as the
- * underlying data structure to store the elements on the stack. 
- * 
- * Not all operations on a stack will always be successful. For
- * example, a programmer may try to pop an element from an empty
- * stack. Since we hace not covered exceptions yet, we need another
- * mechanism to report errors. In order to do that, methods of this
- * list will return a {@see ReturnObject} that will contain either an
- * object or an error value of the right kind (as defined in {@see
- * ErrorMessage}).
- * 
- * @author PiJ
+ * @author svince04
  */
 public class ImprovedStackImpl implements ImprovedStack {
 	/**
-     * The data structure where the elements on the stack are stored. 
+     * The data structure where the elements of the stack are stored. 
      */
     protected List internalList;
 
@@ -24,13 +13,6 @@ public class ImprovedStackImpl implements ImprovedStack {
     	this.internalList = internalList;
     }
 
-	/**
-	 * Returns a copy of this stack with the items reversed, the top
-	 * elements on the original stack is at the bottom of the new
-	 * stack and viceversa.
-	 * 
-	 * @return a copy of this stack with the items reversed. 
-	 */
 	public ImprovedStack reverse() {
 		List revList = new ArrayList();
 			// Populate revList with reversed InternalList
@@ -42,15 +24,6 @@ public class ImprovedStackImpl implements ImprovedStack {
 			return revStack;
 	}
 
-	/**
-	 * Removes the given object from the stack if it is
-	 * there. Multiple instances of the object are all removed.
-	 *
-	 * Classes implementing this method must use method .equals() to
-	 * check whether the item is in the stack or not.
-	 * 
-	 * @param object the object to remove
-	 */
 	public void remove(Object object) {
 		for (int i = 0; i < size(); i++) {
 			if (internalList.get(i).getReturnValue().equals(object)) {
--- src/StackImpl.java
+++ src/StackImpl.java
@@ -11,52 +11,23 @@ public class StackImpl extends AbstractStack {
 	  super(list);
 	}
 
-	/**
-	 * Returns true if the list is empty, false otherwise. 
-	 * 
-	 * @return true if the list is empty, false otherwise. 
-	 */
 	public boolean isEmpty() {
 		return (internalList.isEmpty());
 	}
 
-	/**
-	 * Returns the number of items currently in the list.
-	 * 
-	 * @return the number of items currently in the list
-	 */
 	public int size() {
 		return internalList.size();
 	}
 
-	/**
-	 * Adds an element at the top of the stack. 
-	 * 
-	 * @param item the new item to be added
-	 */
 	public void push(Object item) {
 		internalList.add(0, item);
 	}
 	
-	/**
-	 * Returns the element at the top of the stack. The stack is
-	 * left unchanged.
-	 * 
-	 * @return If stack is not empty, the item on the top is returned. If the
-	 *         stack is empty, an appropriate error.
-	 */
 	public ReturnObject top() {
 		ReturnObject retObject = internalList.get(0);
 		return retObject;
 	}
 
-	/**
-	 * Returns the element at the top of the stack. The element is
-	 * removed from the stack.
-	 * 
-	 * @return If stack is not empty, the item on the top is returned. If the
-	 *         stack is empty, an appropriate error.
-	 */
 	public ReturnObject pop() {
 		ReturnObject retObject = internalList.remove(0);
 		return retObject;
