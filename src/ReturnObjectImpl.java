6843bff390d81830110176b3d14e52508a130754
Sarah <svince04@mail.bbk.ac.uk>
2017-01-08 16:26:16 +0000

    Removes superfluous javadoc

--- src/ReturnObjectImpl.java
+++ src/ReturnObjectImpl.java
@@ -24,10 +24,6 @@ public class ReturnObjectImpl implements ReturnObject {
 		this.object = object;
 	}
 
-	/**
-	 * Returns whether there has been an error
-	 * @return whether there has been an error
-	 */
 	public boolean hasError(){
 		if (errorMessage == ErrorMessage.NO_ERROR || errorMessage == null){
 			return false;
@@ -36,14 +32,6 @@ public class ReturnObjectImpl implements ReturnObject {
 		}
 	}
 
-	/**
-	 * Returns the error message. 
-	 * 
-	 * This method must return NO_ERROR if and only if
-	 * {@hasError} returns false.
-	 * 
-	 * @return the error message
-	 */
 	public ErrorMessage getError(){
 		if (this.hasError()) {
 			return errorMessage;
@@ -52,18 +40,6 @@ public class ReturnObjectImpl implements ReturnObject {
 		}
 	}
 
-	/**
-	 * Returns the object wrapped in this ReturnObject, i.e. the
-	 * result of the operation if it was successful, or null if
-	 * there has been an error.
-	 * 
-	 * Note that the output of this method must be null if {@see
-	 * hasError} returns true, but the opposite is not true: if
-	 * {@see hasError} returns false, this method may or may not
-	 * return null.
-	 * 
-	 * @return the return value from the method or null if there has been an error
-	 */
 	public Object getReturnValue(){
 		if (this.hasError()) {
 			return null;
