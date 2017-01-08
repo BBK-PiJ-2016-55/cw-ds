7497af775e58cd5069ed5a681eaceb7eb91241e1
Sarah <svince04@mail.bbk.ac.uk>
2017-01-08 16:36:17 +0000

    removed superfluous javadoc

--- src/FunctionalLinkedList.java
+++ src/FunctionalLinkedList.java
@@ -10,27 +10,11 @@ public class FunctionalLinkedList extends LinkedList implements FunctionalList {
 		super();
 	}
 
-	/**
-     * Returns the element at the beginning of the list. 
-     * 
-     * If the list is empty, an appropriate error is returned using 
-     * get() from {@see LinkedList}.
-     *
-     * @return a copy of the element at the beginning of the list or 
-     *         an error if the list is empty.
-     */
 	public ReturnObject head() {
 		super.get(0);
 		return retObject;
 	}
 
-    /**
-     * Returns a list with the elements in this list except the
-     * head. The elements must be in the same order. The original list
-     * must not change or be affected by changes in the new list. 
-     * 
-     * If the list is empty, another empty list is returned. 
-     */
 	public FunctionalList rest() {
 		FunctionalList funList = new FunctionalLinkedList();
 		// Check for empty/head-only list
